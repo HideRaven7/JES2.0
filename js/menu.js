6f9dee5746d37cd5a6ddc92878b995e10003024c
albiery <albieryrf@gmail.com>
2024-05-05 22:22:40 -0400

    'pienso que ya vamos mejor'

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@ const menu = document.querySelector('.menu');
 
 menu.addEventListener('click',() => {
     Swal.fire({
-        html: '<ul class="menu-navegador-eme"><li class="li-eme"><a class="a-eme">Configuraci&oacute;n</a></li><li class="li-eme"><a class="a-eme">Reporte</a></li><li class="li-eme"><a class="a-eme">Cerrar</a></li></ul>',
+        html: '<ul><li><a href="#" class="a-eme report-eme">Reporte</a></li><li><a href="#" class="a-eme conf-eme">Configuraci&oacute;n</a></li><li><a href="index.html" class="a-eme cerrar-eme">Cerrar Sesion</a></li></ul>',
         backdrop: true,
         customClass: {
             popup: 'emergente-class classes-eme',
